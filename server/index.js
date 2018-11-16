676923c251f25ab55e9a26333c37e43dd9440e68
Vu Nguyen <trachanh@gmail.com>
2018-11-16 15:17:51 +0200

    connect to DB works

--- server/index.js
+++ server/index.js
@@ -7,8 +7,23 @@ const router = require('./router');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// App setup
 dotenv.config();
+
+// Connect to DB
+
+const DB_USERNAME = process.env.DB_USERNAME || 'invalid_username' ;
+const DB_PASSWORD = process.env.DB_PASSWORD || 'invalid_password';
+const DB_URI = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds061839.mlab.com:61839/shortcut-lottery-lunch`;
+
+mongoose.connect(DB_URI, { useNewUrlParser: true }, function(err) {
+    if(err) {
+        console.log("Connect to DB failed", err.stack);
+    } else {
+        console.log("Connect to DB successfully");
+    }
+})
+
+// App setup
 
 // Morgan is a middleware that logs all requests and response
 // This is for debugging
